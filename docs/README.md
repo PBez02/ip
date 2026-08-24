ce9529d96541d615a8873f17b5a0f0f0a2d1e799
PBez02 <peterbez123@hotmail.com>
2026-08-25 00:35:35 +0800

    Complete Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Zeus User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Zeus.java
@@ -0,0 +1,21 @@
+/**
+ * Entry point for the Zeus chatbot application.
+ */
+public class Zeus {
+    public static void main(String[] args) {
+        String banner = " _____\n"
+                + "|__  /___ _   _ ___\n"
+                + "  / // _ \\ | | / __|\n"
+                + " / /|  __/ |_| \\__ \\\n"
+                + "/____\\___|\\__,_|___/\n";
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Zeus.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
